61a4fe88441c3adeb2e04b881cded607dbd337a7
Alec Wang <alswang18@gmail.com>
2021-01-02 21:51:27 -0700

    finished react setup

--- instahomes/instahomes/settings.py
+++ instahomes/instahomes/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'listings',
     'realtors',
     'accounts',
-    'contacts'
+    'contacts',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -126,11 +127,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'instahomes/static')
-]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
