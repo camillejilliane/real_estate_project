3cf49dccb5ff946a55f89020d66b0af5fd6e21ed
bgsong03 <bgsong03@gmail.com>
2021-01-06 11:34:03 +0800

    modified:   instahomes/frontend/package-lock.json 	modified:   instahomes/frontend/package.json 	modified:   instahomes/frontend/src/components/Footer.jsx 	modified:   instahomes/frontend/src/components/Navbar.jsx

--- instahomes/frontend/src/components/Navbar.jsx
+++ instahomes/frontend/src/components/Navbar.jsx
@@ -5,106 +5,106 @@ import {
     Route,
     Link,
     Redirect,
-  } from "react-router-dom";
+} from "react-router-dom";
 import logo from '../../static/img/logo.png'
 
-  export default function Topbar(){
-    return (
-      <div>
-        <span className="nav">
+
+export default function Navbar(){
+  return (
+    <div>
+      <span className="nav">
         <Router>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-    <div class="container">
-    
-        <Link to="/frontend/" className="navbar-brand">
-        <img src={logo} class="logo" alt=""></img>
-        </Link>
-    
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <ul class="navbar-nav">
-          <li
-            /*{% if '/' == request.path %}
-              class="nav-item active mr-3"
-            {% else %}
-              class="nav-item mr-3"
-            {% endif %}*/
-          >            
-            <Link className="nav-link" to="/frontend/">Home</Link>
-          </li>
-          <li
-            /*{% if 'about' in request.path %}
-              class="nav-item active mr-3"
-            {% else %}
-              class="nav-item mr-3"
-            {% endif %}*/
-          >
-            <a class="nav-link" href="{% url 'about' %}">About</a>
-          </li>
-          <li
-            /*{% if 'listings' in request.path %}
-              class="nav-item active mr-3"
-            {% else %}
-              class="nav-item mr-3"
-            {% endif %}*/
-          >
-            <a class="nav-link" href="{% url 'listings' %}">Featured Listings</a>
-          </li>
-        </ul>
+          <div class="container">
+            <Link to="/frontend/" className="navbar-brand">
+              <img src={logo} class="logo" alt=""></img>
+            </Link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <ul class="navbar-nav">
+                <li
+                  className={}
+                  /*{% if '/' == request.path %}
+                  class="nav-item active mr-3"
+                  {% else %}
+                  class="nav-item mr-3"
+                  {% endif %}*/
+                >            
+                  <Link className="nav-link" to="/frontend/">Home</Link>
+                </li>
+                <li
+                  /*{% if 'about' in request.path %}
+                  class="nav-item active mr-3"
+                  {% else %}
+                  class="nav-item mr-3"
+                  {% endif %}*/
+                >
+                  <Link className="nav-link" to="/frontend/about">About</Link>
+                </li>
+                <li
+                  /*{% if 'listings' in request.path %}
+                  class="nav-item active mr-3"
+                  {% else %}
+                  class="nav-item mr-3"
+                  {% endif %}*/
+                >
+                  <Link className="nav-link" to="/frontend/listings">Featured Listings</Link>
+                </li>
+              </ul>
 
-        <ul class="navbar-nav ml-auto">
-            {/*{% if user.is_authenticated %}*/}
-            <li
-            /*{% if 'dashboard' in request.path %}
-              class="nav-item active mr-3"
-            {% else %}
-              class="nav-item mr-3"
-            {% endif %}*/
-            >
-              <a class="nav-link" href="{% url 'dashboard' %}">
-               Welcome {/*{ user.username }*/} (Dashboard)</a>
-            </li>
-            <li class="nav-item mr-3">
-              <a href="javascript:{document.getElementById('logout').submit()}" class="nav-link">
-                <i class="fas fa-sign-out-alt"></i> Logout
-              </a>
-              <form action="{% url 'logout' %}" method="POST" id="logout">
-                {/*{% csrf_token %}*/}
-                <input type="hidden"></input>
-              </form>
-            </li>
-          {/*{% else %}*/}
-            <li
-            /*{% if 'register' in request.path %}
-              class="nav-item active mr-3"
-            {% else %}
-              class="nav-item mr-3"
-            {% endif %}*/
-            >
-              <a class="nav-link" href="{% url 'register' %}">
-                <i class="fas fa-user-plus"></i> Register</a>
-            </li>
-            <li
-            /*{% if 'login' in request.path %}
-              class="nav-item active mr-3"
-            {% else %}
-              class="nav-item mr-3"
-            {% endif %}*/
-            >
-              <a class="nav-link" href="{% url 'login' %}">
-                <i class="fas fa-sign-in-alt"></i>
+              <ul class="navbar-nav ml-auto">
+                {/*{% if user.is_authenticated %}*/}
+                  <li
+                    /*{% if 'dashboard' in request.path %}
+                    class="nav-item active mr-3"
+                    {% else %}
+                    class="nav-item mr-3"
+                    {% endif %}*/
+                  >
+                    <Link className="nav-link" to="/frontend/dashboard">Welcome {/*{ user.username }*/} (Dashboard)</Link>
+                  </li>
+                  <li class="nav-item mr-3">
+                    <a href="javascript:{document.getElementById('logout').submit()}" class="nav-link">
+                      <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                    <form action="{% url 'logout' %}" method="POST" id="logout">
+                      {/*{% csrf_token %}*/}
+                      <input type="hidden"></input>
+                    </form>
+                  </li>
 
-                Login</a>
-            </li>
-          {/*{% endif %}*/}
-        </ul>
-      </div>
-    </div>
-  </nav>
-  </Router>
-        </span>
-      </div>  
+                {/*{% else %}*/}
+                  <li
+                    /*{% if 'register' in request.path %}
+                    class="nav-item active mr-3"
+                    {% else %}
+                    class="nav-item mr-3"
+                    {% endif %}*/
+                  >
+                    <Link className="nav-link" to="/frontend/register">
+                    <i class="fas fa-user-plus"></i> Register
+                    </Link>
+                  </li>
+                  <li
+                    /*{% if 'login' in request.path %}
+                    class="nav-item active mr-3"
+                    {% else %}
+                    class="nav-item mr-3"
+                    {% endif %}*/
+                  >
+                    <Link className="nav-link" to="/frontend/login">
+                      <i class="fas fa-sign-in-alt"></i>Login
+                    </Link>
+                  </li>
+                {/*{% endif %}*/}
+              </ul>
+            </div>
+          </div>
+        </nav>
+        </Router>
+      </span>
+    </div>  
     );
   }
