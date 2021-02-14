15c04f7137130220fe56371db41f9becaa4d0383
Daniel Del Rio <daniel.delrio@obf.ateneo.edu>
2021-02-14 16:50:56 +0800

    Restructure landing page branch

--- instahomes/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GetListingView, GetRealtorView
-
-urlpatterns = [
-    path('listing', GetListingView.as_view()),
-    path('realtor', GetRealtorView.as_view())
-]
--- instahomes/listings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('', index, name='listings'),
-    path('<int:listing_id>', listing, name='listing'),
-    path('search', search, name='search'),
-]
